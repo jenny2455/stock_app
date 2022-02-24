f80a303cfa2570917b3d674389ccffc96673c8df
Jenny <wasanajclements@gmail.com>
2022-02-24 12:25:40 -0600

    final version

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
-    api_key = 'pk_e25ea171e4bc49b093ea9de578d08b64'
-    @api = StockQuote::Stock.new(api_key: api_key)
+    require_relative '../.api_key.rb'
+    @api = StockQuote::Stock.new(api_key: $api_key)
     
     if params[:ticker] == "" 
       @nothing = "Add a ticker symbol to search."
